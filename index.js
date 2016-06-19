2c820430109b8c60c635a8f1bd32a4488a24cc1b
Alison <alisonmonteiro.10@gmail.com>
2016-06-19 18:12:03 -0300

    kiss and add some keywords on package.json

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 
-module.exports = (baseUrl, opts) => {
+module.exports = (url, opts) => {
   opts = opts || {};
 
-  var params = Object.keys(opts).map(function(item) {
+  let params = Object.keys(opts).map(function(item) {
     return item + '=' + encodeURIComponent(opts[item]);
-  }).join('&');
+  });
 
-  params = params.replace(/%20/g, "+");
+  params = params
+    .join('&')
+    .replace(/%20/g, '+');
 
-  return baseUrl + ((params) ? '?' + params : params);
+  return url + (params ? '?' + params : params);
 }
